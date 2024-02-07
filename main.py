41ad357c322bdca45729b6c7f83bc6d337190561
Gary_ACER <gary061508@gmail.com>
2024-02-07 19:46:39 +0800

    first initialization of repo testing for display, use original file from git reference

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # this is the main of T-pico_C3
+
+import machine
+import utime
+
+# ===== the only LDE on PICO, reserve for LED
+led = machine.Pin(25, machine.Pin.OUT)
+
+state0 = 0
+led.value(0)
+while 1:
+    led.value(state0)
+    utime.sleep_ms(500)
+
+    if state0 == 0:
+        state0 = 1
+    else:
+        state0 = 0
