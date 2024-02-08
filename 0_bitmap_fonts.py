683b63286a98e419e740a5b239dc4c650495d80d
gary_surf7 <garynova781@gmail.com>
2024-02-08 20:51:07 +0800

    update for testing

--- 0_bitmap_fonts.py
+++ 0_bitmap_fonts.py
@@ -13,7 +13,14 @@ import vga1_8x16 as font2
 import vga1_bold_16x16 as font3
 
 # import vga1_bold_16x32 as font4
-import vga2_16x32 as font4
+import vga1_16x16 as font4
+import vga1_16x32 as font5
+import vga2_8x8 as font6
+import vga2_8x16 as font7
+import vga2_16x16 as font8
+import vga2_16x32 as font9
+import vga2_bold_16x32 as font10
+import vga2_bold_16x16 as font11
 
 
 tft = tft_config.config(0)
@@ -23,7 +30,7 @@ def main():
     tft.init()
 
     while True:
-        for font in (font1, font2, font3, font4):
+        for font in (font1, font2, font3, font4, font5, font6, font7, font8, font9, font10, font11):
             tft.fill(st7789.BLUE)
             line = 0
             col = 0
